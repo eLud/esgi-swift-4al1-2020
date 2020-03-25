cf2f946bbbf0eab1951595c95e3b5220e0077b3c
Ludovic Ollagnier <ludovic@tectec.fr>
2020-03-25 10:05:56 +0100

    Added enum and restaurant class

--- Playgrounds/AppModel.playground/Contents.swift
+++ Playgrounds/AppModel.playground/Contents.swift
@@ -2,10 +2,14 @@ import UIKit
 
 struct Meal {
 
+    enum Category {
+        case first, main, desert, drink
+    }
+
     var name: String
     var price: Double
     var description: String
-    var category: String
+    var category: Category
     var components: [Aliment]
     var allergens: [String] {
         #warning("Don't forget the allergens")
@@ -24,7 +28,36 @@ struct Meal {
 
 struct Aliment {
     let name: String
-    let allergens: [String]
+    let allergens: [Allergen]
     let isVegan: Bool
     let isVeggie: Bool
+}
+
+enum Allergen {
+    case gluten
+    case fish
+    case lactose
+}
+
+class Restaurant {
+
+    let name: String
+    private var menu: [Meal]
+
+    init(name: String) {
+        self.name = name
+        menu = []
+    }
+
+    func add(_ meal: Meal) {
+        menu.append(meal)
+    }
+
+    func remove(_ meal: Meal) {
+
+    }
+
+    func all() -> [Meal] {
+        return menu
+    }
 }
