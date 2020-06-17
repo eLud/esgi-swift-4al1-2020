6d046b164d8317e51e2ad76661538e879bebf995
Ludovic Ollagnier <ludovic@tectec.fr>
2020-06-17 13:03:48 +0200

    Updated meal app

--- Projects/MealApp/MealApp/ContentView.swift
+++ Projects/MealApp/MealApp/ContentView.swift
@@ -13,8 +13,35 @@ struct ContentView: View {
     @State var meal = Meal(name: "Pizza", price: 11.5, pitch: "Une spécialité italienne", category: .main, components: [Aliment(name: "Farine", allergens: [.gluten], isVegan: true, isVeggie: true), Aliment(name: "Tomate", allergens: [], isVegan: true, isVeggie: true), Aliment(name: "Fromage", allergens: [.lactose], isVegan: false, isVeggie: true)], calories: nil)
 
     var body: some View {
-        VStack {
-            Text(meal.name)
+        VStack(alignment: .leading) {
+            Image("pizza")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Text("Plat")
+                .padding([.horizontal], 10)
+                .padding([.vertical], 2)
+                .background(
+                    RoundedRectangle(cornerRadius: 10).foregroundColor(.green))
+            HStack(alignment: .firstTextBaseline) {
+                Text(meal.name)
+                    .font(.title)
+                Spacer()
+                Text("\(meal.price)€")
+                    .font(.headline)
+                    .foregroundColor(.gray)
+            }.padding()
+            VStack(alignment: .leading) {
+                Text(meal.pitch)
+                if meal.components != nil {
+                    Text("Ingredients:")
+                        .font(.headline)
+                        .padding([.top], 8)
+                    ForEach(meal.components!, id: \.name) { (aliment) in
+                        Text(aliment.name)
+                    }
+                }
+            }.padding()
+            Spacer()
         }
     }
 }
